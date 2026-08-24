954377c66ed9395c9f843a7c871ae3b77dd0de31
Ritesh Pandit <PanditRitesh@JohnDeere.com>
2026-08-24 13:52:43 +0530

    Fix APP_VERSION mismatch for v2.1.1 release

--- ff_downloader/__init__.py
+++ ff_downloader/__init__.py
@@ -1,3 +1,3 @@
 """Fucking Fast Downloader Python application."""
 
-__version__ = "2.0.0"
+__version__ = "2.1.1"
--- ff_downloader/config.py
+++ ff_downloader/config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 APP_NAME = "Fucking Fast Downloader"
-APP_VERSION = "2.1.0"
+APP_VERSION = "2.1.1"
 DOWNLOADS_DIR = Path.home() / "Downloads" / "Fucking Fast Downloader"
 DOWNLOADS_DIR.mkdir(parents=True, exist_ok=True)
 
